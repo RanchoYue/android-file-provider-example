6515343bd0fb627bccbab6398fa876e582d6a594
Iddan Givon <givonid@gmail.com>
2019-04-20 14:50:07 +0300

    working example copy file to external folder and open it using file provider

--- app/src/main/java/com/idg/fileproviderexample/MainActivity.java
+++ app/src/main/java/com/idg/fileproviderexample/MainActivity.java
@@ -1,13 +1,28 @@
 package com.idg.fileproviderexample;
 
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+import static android.provider.ContactsContract.Directory.PACKAGE_NAME;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +37,84 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String pdfFileName = "FileProviderExample.pdf";
+                String pdfFolderInAssets = "pdf";
+
+                //1st step, copy from assets to external files folder
+                copyAssets(pdfFolderInAssets);
+
+                //2nd step, sent intent with our content file provider uri to open file with external pdf app
+                displayFile(pdfFileName);
             }
         });
     }
+
+
+    private void copyAssets(String folderInsideAssets) {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list(folderInsideAssets);
+        } catch (IOException e) {
+            Log.e("tag", "Failed to get asset file list.", e);
+        }
+        if (files != null) for (String filename : files) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = assetManager.open(folderInsideAssets +"/" + filename);
+                File outFile = new File(getExternalFilesDir(null), filename);
+                copyFile(in,outFile);
+
+            } catch(IOException e) {
+                Log.e("tag", "Failed to copy asset file: " + filename, e);
+            }
+            finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        // NOOP
+                    }
+                }
+            }
+        }
+    }
+
+    private void copyFile(InputStream in, File dst) throws IOException {
+        FileOutputStream out=new FileOutputStream(dst);
+        byte[] buf=new byte[1024];
+        int len;
+
+        while ((len=in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+
+        in.close();
+        out.close();
+    }
+
+    private void displayFile(String filename) {
+        try {
+            //get the file from external files
+            File newFile = new File(getExternalFilesDir(null).getAbsolutePath() + "/" + filename);
+            //create the URI for our file provider
+            Uri fileUri = FileProvider.getUriForFile(this,
+                    BuildConfig.APPLICATION_ID + ".provider", newFile);
+
+            getApplicationContext().grantUriPermission(PACKAGE_NAME,
+                    fileUri, FLAG_GRANT_READ_URI_PERMISSION | FLAG_GRANT_WRITE_URI_PERMISSION);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(fileUri, "application/pdf");
+            intent.setFlags(FLAG_GRANT_READ_URI_PERMISSION | FLAG_GRANT_WRITE_URI_PERMISSION);
+            startActivity(intent);
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.e("tag", "Failed to open file: " + filename, e);
+            Toast.makeText(this, "df "+e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
